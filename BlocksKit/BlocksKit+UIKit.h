ced704c2057a0898f853ed0c8c49683c164221ec
tanaka.kenji <tanaka.kenji@ip-172-17-0-130.ap-northeast-1.compute.internal>
2017-05-08 10:40:21 +0900

    不要なimportを削除

--- BlocksKit/BlocksKit+UIKit.h
+++ BlocksKit/BlocksKit+UIKit.h
@@ -25,19 +25,11 @@
 //  THE SOFTWARE.
 //
 
-#import <BlocksKit/BlocksKit.h>
-
 #import <BlocksKit/UIActionSheet+BlocksKit.h>
 #import <BlocksKit/UIAlertView+BlocksKit.h>
 #import <BlocksKit/UIBarButtonItem+BlocksKit.h>
 #import <BlocksKit/UIControl+BlocksKit.h>
 #import <BlocksKit/UIGestureRecognizer+BlocksKit.h>
-#import <BlocksKit/UIImage+BlocksKit.h>
-#import <BlocksKit/UIImagePickerController+BlocksKit.h>
 #import <BlocksKit/UIPopoverController+BlocksKit.h>
-#import <BlocksKit/UITextField+BlocksKit.h>
 #import <BlocksKit/UIView+BlocksKit.h>
 #import <BlocksKit/UIWebView+BlocksKit.h>
-#import <BlocksKit/UITextField+BlocksKit.h>
-#import <BlocksKit/UITextView+BlocksKit.h>
-#import <BlocksKit/UIImagePickerController+BlocksKit.h>
